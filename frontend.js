3d60f099bfd072991ac492a77abd89a795cb48b0
TanviKumar <tanvikumar777@gmail.com>
2018-01-13 19:30:40 +0530

    Add bounce and tagline

--- frontend.js
+++ frontend.js
@@ -8,6 +8,11 @@ var aaveg;
 var broken = false;
 var sizeVar = 24;
 var x = 0, y = 0;
+var bounced = false;
+var complete = false;
+var back_up = 0;
+var back_down = 0;
+var rock_diff = 24;
 
 function setup() {
 	createCanvas(windowWidth,windowHeight); 
@@ -23,6 +28,7 @@ function setup() {
     left_rock = loadImage("imgs/left_rock.png");
     broken_rock = loadImage("imgs/broken_rock.png");
     aaveg = loadImage("imgs/Layer2.png");
+    carved = loadImage("imgs/carved.png");
     frameRate(60);
 }
 
@@ -32,18 +38,36 @@ function draw() {
     noStroke();
 	rect(0,0,windowWidth, windowHeight);
 	background(bg_image);
-	if(iterations*10 < windowHeight*2/5) {
-		image(full_rock, windowWidth/2 - windowWidth/3, iterations*10, windowWidth/2, windowHeight*2/3);
+	if(iterations*20 < windowHeight*3/5) {
+		image(full_rock, windowWidth/2 - windowWidth/8, iterations*20, windowWidth/4, windowHeight/3);
 		
 	}
-	else if (iterations*10 == windowHeight*2/5){
-		image(broken_rock, windowWidth/2 - broken_rock.width/2, windowHeight/2 - broken_rock.height/4);
-		broken = true;
+	else if (iterations*20 >= windowHeight*3/5  && bounced == false && complete == false){
+		//image(broken_rock, windowWidth/2 - broken_rock.width/2, windowHeight/2 - broken_rock.height/4);
+		bounced = true;
 	}
-	else if (iterations*10 > windowHeight*2/5){
-		image(left_rock, windowWidth/2 - left_rock.width/2 - left_rock.width/sizeVar, windowHeight*3/4 - left_rock.height/2);
-		image(right_rock, windowWidth/2 - right_rock.width/2 + right_rock.width/sizeVar, windowHeight*3/4 - right_rock.height/2);
-		image(aaveg, windowWidth/2 - aaveg.width/sizeVar, windowHeight*3/4 - aaveg.height/sizeVar, aaveg.width*2/sizeVar, aaveg.height*2/sizeVar);
+	else if (bounced == true && complete == false) {
+		if (back_up <= windowHeight/12){
+			image(full_rock, windowWidth/2 - windowWidth/8, windowHeight*2/5 - back_up, windowWidth/4, windowHeight/3);
+			back_up += 10;
+		}
+		else if (back_down <= windowHeight/4){
+			image(full_rock, windowWidth/2 - windowWidth/8, windowHeight*2/5 - back_up + back_down, windowWidth/4, windowHeight/3);
+			back_down += 10;
+		}
+		else {
+			complete = true;
+		}
+	}
+	else if (complete == true){
+		if(rock_diff>17) {
+			image(left_rock, windowWidth/2 - left_rock.width/6 - left_rock.width/rock_diff, windowHeight*3/4 - left_rock.height/6, left_rock.width/3, left_rock.height/3);
+			image(right_rock, windowWidth/2 - right_rock.width/6 + right_rock.width/rock_diff, windowHeight*3/4 - right_rock.height/6, right_rock.width/3, right_rock.height/3);
+			if(rock_diff - 1 > 17)
+				rock_diff --;
+		}
+		image(aaveg, windowWidth/2 - aaveg.width/sizeVar, windowHeight/2 - aaveg.height/sizeVar, aaveg.width*2/sizeVar, aaveg.height*2/sizeVar);
+		image(carved, windowWidth/2 - carved.width/sizeVar, windowHeight*3/4 - carved.height/sizeVar, carved.width*2/sizeVar, carved.height*2/sizeVar);
 		if(sizeVar>10)
 			sizeVar--;
 		else
